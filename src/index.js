3dddf12aa0fadf49d31c4e8bd1dc9b8e6b131be0
Aditya Tripathi <adityakumartripathi6285@gmail.com>
2025-12-11 16:10:06 +0530

    add the crawling functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("Hello world");
+import fetch from "node-fetch";
+import * as cheerio from 'cheerio'
+
+const seenURLs = {};
+
+const getUrl = (link) => {
+    if(link.includes('http')){
+        return link;
+    }else if(link.startsWith("/")){
+        return `http://localhost:8080${link}`;
+    }else{
+        return `http://localhost:8080/${link}`;
+    }
+}
+
+const crawl = async ({url}) => {
+    if(seenURLs[url]) return;
+    console.log('Crawling',url);
+    seenURLs[url] = true;
+
+    const response = await fetch(url)
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const links = $("a")
+        .map((i, link) => link.attribs.href)
+        .get();
+
+    const images = $("img")
+        .map((i, link) => link.attribs.src)
+        .get();
+    console.log("images", images);
+    
+
+    links.forEach(link => {
+        crawl({
+            url:getUrl(link),
+        })
+    })
+}
+
+crawl({
+    url: "http://127.0.0.1:8080/index.html"
+})
